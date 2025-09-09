6f24d072c19c4922df8297d94f97dedac99da78e
cbrra <98268014+Cbrra@users.noreply.github.com>
2025-09-09 22:10:43 +0200

    fix: orion urls

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,17 +3,17 @@ export default {
     ownerIds: ["619838036846575617", "755054105713704960"],
     teamMembers: [
         {
-            id: "619838036846575617",
-            username: "plume64",
-        },
-        {
             id: "755054105713704960",
             username: "lotus64",
+        },
+        {
+            id: "619838036846575617",
+            username: "plume64",
         },
     ],
     supportGuildId: "1306734190238371860",
     ticketChannelId: "1307061230145507410",
-    domain: "orion.lmcgroup.xyz",
-    domainURL: "https://orion.lmcgroup.xyz",
-    panelURL: "https://panel.orion.lmcgroup.xyz",
+    domain: "orionhost.xyz",
+    domainURL: "https://orionhost.xyz",
+    panelURL: "https://panel.orionhost.xyz",
 };
