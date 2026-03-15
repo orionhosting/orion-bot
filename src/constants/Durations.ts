30b221248e6a05a5193c4ccfce78d8af74c2565a
cbrra <98268014+Cbrra@users.noreply.github.com>
2026-03-15 01:22:20 +0100

    feat: replace Durations by time module

--- src/constants/Durations.ts
+++
@@ -1,8 +0,0 @@
-export class Durations {
-    public static readonly YEAR = 31_536_000_000;
-    public static readonly MONTH = 2_592_000_000;
-    public static readonly WEEK = 604_800_000;
-    public static readonly DAY = 86_400_000;
-    public static readonly HOUR = 3_600_000;
-    public static readonly MINUTE = 60_000;
-}
