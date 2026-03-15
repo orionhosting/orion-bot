0c726eb1e935635e83ef7c2773199d97af66a0e1
cbrra <98268014+Cbrra@users.noreply.github.com>
2026-03-15 01:27:18 +0100

    feat: cache

---
+++ src/structures/cache.ts
@@ -0,0 +1,36 @@
+import { ValueCache } from "@sodiumlabs/cache";
+import { config } from "@/config";
+
+/**
+ * The Orion documentation sitemap.
+ */
+type Sitemap = {
+    url: string;
+    lang: string;
+    name: string;
+    description: string;
+}[];
+
+/**
+ * Global cache.
+ */
+export class Cache {
+    /**
+     * Cache of the documentation sitemap.
+     */
+    private readonly documentationSitemapCache = new ValueCache<Sitemap>({ ttl: 600_000 });
+
+    /**
+     * Get the cached Orion documentation sitemap.
+     */
+    public async getDocumentationSitemap() {
+        const cached = this.documentationSitemapCache.get();
+        if (cached) return cached;
+
+        const res = await fetch(`${config.docsURL}/api/pages`);
+        const sitemap = (await res.json()) as Sitemap;
+
+        this.documentationSitemapCache.set(sitemap);
+        return sitemap;
+    }
+}
