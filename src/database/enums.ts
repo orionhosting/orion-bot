561fc56fc94b224b80d7bbbc58fc85562df14d86
cbrra <98268014+Cbrra@users.noreply.github.com>
2026-03-15 01:22:02 +0100

    feat: prisma db files

---
+++ src/database/enums.ts
@@ -0,0 +1,10 @@
+export enum CreditTransactionType {
+    Custom = 0,
+    Giveaway = 1,
+    ReferralCode = 2,
+    SupportActivity = 3,
+    Partnership = 4,
+    DiscordSponsoredAd = 5,
+    PromotionCode = 6,
+    DiscordBoost = 7,
+}
--- src/database/models/Manager.ts
+++
@@ -1,9 +0,0 @@
-import { Schema, model, type InferSchemaType, type HydratedDocument } from "mongoose";
-
-const schema = new Schema({});
-
-export type ManagerSchemaDefinition = InferSchemaType<typeof schema>;
-
-export type Manager = HydratedDocument<ManagerSchemaDefinition>;
-
-export const ManagerModel = model("Manager", schema);
